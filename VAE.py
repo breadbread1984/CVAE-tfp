2e640ceb90c5d2b19851ee25b830f99b73854ed8
breadbread1984 <breadbread1984@163.com>
2019-08-18 19:30:50 +0800

    update VAE to functional api implemented

--- VAE.py
+++ VAE.py
@@ -4,114 +4,57 @@ import cv2;
 import tensorflow as tf;
 import tensorflow_probability as tfp;
 
-class Encoder(tf.keras.Model):
-    
-    def __init__(self, encode_size = 16,  base_depth = 32):
-        
-        # encode_size: latent variable dimension
-        super(Encoder,self).__init__();
-        
-        self.prior = tfp.distributions.Independent(tfp.distributions.Normal(loc = tf.zeros(encode_size), scale = 1), reinterpreted_batch_ndims = 1);
-        
-        self.normalize = tf.keras.layers.Lambda(lambda x: tf.cast(x,tf.float32) - 0.5);
-        self.conv1 = tf.keras.layers.Conv2D(filters = base_depth, kernel_size = (5,5), strides = (1,1), padding = 'same');
-        self.bn1 = tf.keras.layers.BatchNormalization();
-        self.relu1 = tf.keras.layers.LeakyReLU();
-        self.conv2 = tf.keras.layers.Conv2D(filters = base_depth, kernel_size = (5,5), strides = (2,2), padding = 'same');
-        self.bn2 = tf.keras.layers.BatchNormalization();
-        self.relu2 = tf.keras.layers.LeakyReLU();
-        self.conv3 = tf.keras.layers.Conv2D(filters = 2 * base_depth, kernel_size = (5,5), strides = (1,1), padding = 'same');
-        self.bn3 = tf.keras.layers.BatchNormalization();
-        self.relu3 = tf.keras.layers.LeakyReLU();
-        self.conv4 = tf.keras.layers.Conv2D(filters = 2 * base_depth, kernel_size = (5,5), strides = (2,2), padding = 'same');
-        self.bn4 = tf.keras.layers.BatchNormalization();
-        self.relu4 = tf.keras.layers.LeakyReLU();
-        self.conv5 = tf.keras.layers.Conv2D(filters = 4 * base_depth, kernel_size = (7,7), strides = (1,1), padding = 'valid');
-        self.bn5 = tf.keras.layers.BatchNormalization();
-        self.relu5 = tf.keras.layers.LeakyReLU();
-        self.flatten = tf.keras.layers.Flatten();
-        self.dense = tf.keras.layers.Dense(units = tfp.layers.MultivariateNormalTriL.params_size(encode_size));
-        self.gaussian = tfp.layers.MultivariateNormalTriL(encode_size, activity_regularizer = tfp.layers.KLDivergenceRegularizer(self.prior, weight=1.0));
-        
-    def call(self, inputs):
-        
-        result = self.normalize(inputs);
-        result = self.conv1(result);
-        result = self.bn1(result);
-        result = self.relu1(result);
-        result = self.conv2(result);
-        result = self.bn2(result);
-        result = self.relu2(result);
-        result = self.conv3(result);
-        result = self.bn3(result);
-        result = self.relu3(result);
-        result = self.conv4(result);
-        result = self.bn4(result);
-        result = self.relu4(result);
-        result = self.conv5(result);
-        result = self.bn5(result);
-        result = self.relu5(result);
-        result = self.flatten(result);
-        result = self.dense(result);
-        result = self.gaussian(result);
-        assert isinstance(result, tfp.distributions.Distribution);
-        return result;
+def Encoder(input_shape, encode_size = 16, base_depth = 32):
 
-class Decoder(tf.keras.Model):
-    
-    def __init__(self, input_shape = (28, 28, 1), encode_size = 16, base_depth = 32):
-        
-        super(Decoder,self).__init__();
-        
-        self.reshape = tf.keras.layers.Reshape([1, 1, encode_size]);
-        self.conv1 = tf.keras.layers.Conv2DTranspose(filters = 2 * base_depth, kernel_size = (7,7), strides = (1,1), padding = 'valid');
-        self.bn1 = tf.keras.layers.BatchNormalization();
-        self.relu1 = tf.keras.layers.LeakyReLU();
-        self.conv2 = tf.keras.layers.Conv2DTranspose(filters = 2 * base_depth, kernel_size = (5,5), strides = (1,1), padding = 'same');
-        self.bn2 = tf.keras.layers.BatchNormalization();
-        self.relu2 = tf.keras.layers.LeakyReLU();
-        self.conv3 = tf.keras.layers.Conv2DTranspose(filters = 2 * base_depth, kernel_size = (5,5), strides = (2,2), padding = 'same');
-        self.bn3 = tf.keras.layers.BatchNormalization();
-        self.relu3 = tf.keras.layers.LeakyReLU();
-        self.conv4 = tf.keras.layers.Conv2DTranspose(filters = base_depth, kernel_size = (5,5), strides = (1,1), padding = 'same');
-        self.bn4 = tf.keras.layers.BatchNormalization();
-        self.relu4 = tf.keras.layers.LeakyReLU();
-        self.conv5 = tf.keras.layers.Conv2DTranspose(filters = base_depth, kernel_size = (5,5), strides = (2,2), padding = 'same');
-        self.bn5 = tf.keras.layers.BatchNormalization();
-        self.relu5 = tf.keras.layers.LeakyReLU();
-        self.conv6 = tf.keras.layers.Conv2DTranspose(filters = base_depth, kernel_size = (5,5), strides = (1,1), padding = 'same');
-        self.bn6 = tf.keras.layers.BatchNormalization();
-        self.relu6 = tf.keras.layers.LeakyReLU();
-        self.conv7 = tf.keras.layers.Conv2D(filters = 1, kernel_size = (5,5), strides = (1,1), padding = 'same');
-        self.flatten = tf.keras.layers.Flatten();
-        self.bernoulli = tfp.layers.IndependentBernoulli(event_shape = input_shape, convert_to_tensor_fn = tfp.distributions.Bernoulli.logits);
-        
-    def call(self, inputs):
-        
-        result = self.reshape(inputs);
-        result = self.conv1(result);
-        result = self.bn1(result);
-        result = self.relu1(result);
-        result = self.conv2(result);
-        result = self.bn2(result);
-        result = self.relu2(result);
-        result = self.conv3(result);
-        result = self.bn3(result);
-        result = self.relu3(result);
-        result = self.conv4(result);
-        result = self.bn4(result);
-        result = self.relu4(result);
-        result = self.conv5(result);
-        result = self.bn5(result);
-        result = self.relu5(result);
-        result = self.conv6(result);
-        result = self.bn6(result);
-        result = self.relu6(result);
-        result = self.conv7(result);
-        result = self.flatten(result);
-        result = self.bernoulli(result);
-        assert isinstance(result, tfp.distributions.Distribution);
-        return result;
+    inputs = tf.keras.Input(input_shape);
+    results = tf.keras.layers.Lambda(lambda x: tf.cast(x,tf.float32) - 0.5)(inputs);
+    results = tf.keras.layers.Conv2D(filters = base_depth, kernel_size = (5,5), strides = (1,1), padding = 'same')(results);
+    results = tf.keras.layers.BatchNormalization()(results);
+    results = tf.keras.layers.LeakyReLU()(results);
+    results = tf.keras.layers.Conv2D(filters = base_depth, kernel_size = (5,5), strides = (2,2), padding = 'same')(results);
+    results = tf.keras.layers.BatchNormalization()(results);
+    results = tf.keras.layers.LeakyReLU()(results);
+    results = tf.keras.layers.Conv2D(filters = 2 * base_depth, kernel_size = (5,5), strides = (1,1), padding = 'same')(results);
+    results = tf.keras.layers.BatchNormalization()(results);
+    results = tf.keras.layers.LeakyReLU()(results);
+    results = tf.keras.layers.Conv2D(filters = 2 * base_depth, kernel_size = (5,5), strides = (2,2), padding = 'same')(results);
+    results = tf.keras.layers.BatchNormalization()(results);
+    results = tf.keras.layers.LeakyReLU()(results);
+    results = tf.keras.layers.Conv2D(filters = 4 * base_depth, kernel_size = (7,7), strides = (1,1), padding = 'valid')(results);
+    results = tf.keras.layers.BatchNormalization()(results);
+    results = tf.keras.layers.LeakyReLU()(results);
+    results = tf.keras.layers.Flatten()(results);
+    results = tf.keras.layers.Dense(units = tfp.layers.MultivariateNormalTriL.params_size(encode_size))(results);
+    prior = tfp.distributions.Independent(tfp.distributions.Normal(loc = tf.zeros(encode_size), scale = 1), reinterpreted_batch_ndims = 1);
+    results = tfp.layers.MultivariateNormalTriL(encode_size, activity_regularizer = tfp.layers.KLDivergenceRegularizer(prior, weight = 1.0))(results);
+    return tf.keras.Model(inputs = inputs, outputs = results);
+
+def Decoder(output_shape = (28, 28, 1), encode_size = 16, base_depth = 32):
+
+    inputs = tf.keras.Input((encode_size,));
+    results = tf.keras.layers.Reshape((1, 1, encode_size))(inputs);
+    results = tf.keras.layers.Conv2DTranspose(filters = 2 * base_depth, kernel_size = (7,7), strides = (1,1), padding = 'valid')(results);
+    results = tf.keras.layers.BatchNormalization()(results);
+    results = tf.keras.layers.LeakyReLU()(results);
+    results = tf.keras.layers.Conv2DTranspose(filters = 2 * base_depth, kernel_size = (5,5), strides = (1,1), padding = 'same')(results);
+    results = tf.keras.layers.BatchNormalization()(results);
+    results = tf.keras.layers.LeakyReLU()(results);
+    results = tf.keras.layers.Conv2DTranspose(filters = 2 * base_depth, kernel_size = (5,5), strides = (2,2), padding = 'same')(results);
+    results = tf.keras.layers.BatchNormalization()(results);
+    results = tf.keras.layers.LeakyReLU()(results);
+    results = tf.keras.layers.Conv2DTranspose(filters = base_depth, kernel_size = (5,5), strides = (1,1), padding = 'same')(results);
+    results = tf.keras.layers.BatchNormalization()(results);
+    results = tf.keras.layers.LeakyReLU()(results);
+    results = tf.keras.layers.Conv2DTranspose(filters = base_depth, kernel_size = (5,5), strides = (2,2), padding = 'same')(results);
+    results = tf.keras.layers.BatchNormalization()(results);
+    results = tf.keras.layers.LeakyReLU()(results);
+    results = tf.keras.layers.Conv2DTranspose(filters = base_depth, kernel_size = (5,5), strides = (1,1), padding = 'same')(results);
+    results = tf.keras.layers.BatchNormalization()(results);
+    results = tf.keras.layers.LeakyReLU()(results);
+    results = tf.keras.layers.Conv2D(filters = 1, kernel_size = (5,5), strides = (1,1), padding = 'same')(results);
+    results = tf.keras.layers.Flatten()(results);
+    results = tfp.layers.IndependentBernoulli(event_shape = output_shape, convert_to_tensor_fn = tfp.distributions.Bernoulli.logits)(results);
+    return tf.keras.Model(inputs = inputs, outputs = results);
 
 class VAE(tf.keras.Model):
     
@@ -121,29 +64,26 @@ class VAE(tf.keras.Model):
         
         self.prior = tfp.distributions.Independent(tfp.distributions.Normal(loc = tf.zeros(encode_size), scale = 1));
         
-        self.encoder = Encoder(encode_size, base_depth);
+        self.encoder = Encoder(input_shape, encode_size, base_depth);
         self.decoder = Decoder(input_shape, encode_size, base_depth);
     
-    def call(self, inputs, weight = 1.0):
+    def call(self, inputs):
         
-        code_distr = self.encoder(inputs);
-        sample_distr = self.decoder(code_distr);
-        likelihood_loss = -sample_distr.log_prob(inputs);
-
-        return likelihood_loss;
+        code = self.encoder(inputs);
+        sample = self.decoder(code);
+        return sample;
     
     def sample(self, batch_size = 1):
         
-        code = self.prior.sample(sample_shape = (batch_size));
-        sample_distr = self.decoder(code);
-        return sample_distr.sample();
+        code = self.prior.sample(sample_shape = (batch_size,));
+        sample = self.decoder(code);
+        return sample.sample();
 
 if __name__ == "__main__":
     
     optimizer = tf.keras.optimizers.Adam(1e-4);
     vae = VAE();
-    checkpoint = tf.train.Checkpoint(model = vae, optimizer = optimizer, optimizer_step = optimizer.iterations);
-    checkpoint.restore(tf.train.latest_checkpoint('vae_checkpoints'));
+    vae.load_weights('vae.h5');
     for i in range(10):
         img = vae.sample();
         img = img[0,...].numpy() * 255.0;
--- train_VAE.py
+++ train_VAE.py
@@ -3,52 +3,28 @@
 import os;
 import tensorflow as tf;
 import tensorflow_datasets as tfds;
-import tensorflow_probability as tfp;
 from VAE import VAE;
 
-batch_size = 100;
+batch_size = 256;
 
 def parse_function(feature):
     data = feature["image"];
     data = tf.cast(data,dtype = tf.float32) / 255.;
     label = feature["label"];
-    return data,label;
+    return data, data;
 
 def main():
     
     vae = VAE();
-    optimizer = tf.keras.optimizers.Adam(1e-3);
     #load dataset
     trainset = tfds.load(name = "mnist", split = tfds.Split.TRAIN, download = False);
-    trainset = trainset.map(parse_function).shuffle(batch_size).batch(batch_size);
+    trainset = trainset.map(parse_function).shuffle(batch_size).batch(batch_size).prefetch(tf.data.experimental.AUTOTUNE);
     testset = tfds.load(name = "mnist", split = tfds.Split.TRAIN, download = False);
-    testset = testset.map(parse_function).shuffle(batch_size).batch(batch_size);
-    #restore from existing checkpoint
-    if False == os.path.exists('vae_checkpoints'): os.mkdir('vae_checkpoints');
-    checkpoint = tf.train.Checkpoint(model = vae, optimizer = optimizer, optimizer_step = optimizer.iterations);
-    checkpoint.restore(tf.train.latest_checkpoint('vae_checkpoints'));
-    #create log
-    log = tf.summary.create_file_writer('vae_checkpoints');
-    #train model
-    print('training');
-    avg_loss = tf.keras.metrics.Mean(name = 'loss', dtype = tf.float32);
-    while True:
-        for (images, labels) in trainset:
-            with tf.GradientTape() as tape:
-                loss = vae(images);
-                avg_loss.update_state(loss);
-            #write log
-            if tf.equal(optimizer.iterations % 100, 0):
-                with log.as_default():
-                    tf.summary.scalar('loss',avg_loss.result(), step = optimizer.iterations);
-                    for i in range(10):
-                        tf.summary.image(str(i),vae.sample(), step = optimizer.iterations);
-                print('Step #%d Loss: %.6f' % (optimizer.iterations, avg_loss.result()));
-                avg_loss.reset_states();
-            grads = tape.gradient(loss, vae.trainable_variables);
-            optimizer.apply_gradients(zip(grads, vae.trainable_variables));
-        #save check point
-        checkpoint.save(os.path.join('vae_checkpoints','ckpt'));
+    testset = testset.map(parse_function).shuffle(batch_size).batch(batch_size).prefetch(tf.data.experimental.AUTOTUNE);
+
+    vae.compile(optimizer = tf.keras.optimizers.Adam(1e-3), loss = lambda x, sample: -sample.log_prob(x));
+    vae.fit(trainset, epochs = 15, validation_data = testset);
+    vae.save_weights('vae.h5');
 
 if __name__ == "__main__":
     
